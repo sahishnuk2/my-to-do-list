74b6e57ade8af5b2a42a10f0b567b5084c5bf00e
Sahishnu <sahishnuk2@gmail.com>
2025-08-06 16:32:51 +0800

    static design ready (not split into components yet)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,180 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <h1>To Do List</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="pending">
+        <p>Hi Sahishnu, you have no pending tasks left</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="tasks">
+        <h2>Tasks</h2>
+        <div className="subject">
+          <table>
+            <thead>
+              <tr>
+                <th colSpan={5}>CS2100</th>
+              </tr>
+              <tr>
+                <th>Name</th>
+                <th>Deadline</th>
+                <th>Priority</th>
+                <th>Progress</th>
+                <th>isCompleted</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Task 1</td>
+                <td>25/08/25</td>
+                <td>High</td>
+                <td>Not Started</td>
+                <td>False</td>
+              </tr>
+              <tr>
+                <td>Task 2</td>
+                <td>25/08/25</td>
+                <td>High</td>
+                <td>In Progress</td>
+                <td>False</td>
+              </tr>
+              <tr>
+                <td>Task 3</td>
+                <td>25/08/25</td>
+                <td>High</td>
+                <td>Finished</td>
+                <td>True</td>
+              </tr>
+            </tbody>
+          </table>
+          <button className="add">Add Task</button>
+        </div>
+        <div className="subject">
+          <table>
+            <thead>
+              <tr>
+                <th colSpan={5}>ACC1701X</th>
+              </tr>
+              <tr>
+                <th>Name</th>
+                <th>Deadline</th>
+                <th>Priority</th>
+                <th>Progress</th>
+                <th>isCompleted</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Task 1</td>
+                <td>25/08/25</td>
+                <td>High</td>
+                <td>Not Started</td>
+                <td>False</td>
+              </tr>
+              <tr>
+                <td>Task 2</td>
+                <td>25/08/25</td>
+                <td>High</td>
+                <td>In Progress</td>
+                <td>False</td>
+              </tr>
+              <tr>
+                <td>Task 3</td>
+                <td>25/08/25</td>
+                <td>High</td>
+                <td>Finished</td>
+                <td>True</td>
+              </tr>
+            </tbody>
+          </table>
+          <button className="add">Add Task</button>
+        </div>
+        <div className="subject">
+          <table>
+            <thead>
+              <tr>
+                <th colSpan={5}>CS2103T</th>
+              </tr>
+              <tr>
+                <th>Name</th>
+                <th>Deadline</th>
+                <th>Priority</th>
+                <th>Progress</th>
+                <th>isCompleted</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Task 1</td>
+                <td>25/08/25</td>
+                <td>High</td>
+                <td>Not Started</td>
+                <td>False</td>
+              </tr>
+              <tr>
+                <td>Task 2</td>
+                <td>25/08/25</td>
+                <td>High</td>
+                <td>In Progress</td>
+                <td>False</td>
+              </tr>
+              <tr>
+                <td>Task 3</td>
+                <td>25/08/25</td>
+                <td>High</td>
+                <td>Finished</td>
+                <td>True</td>
+              </tr>
+            </tbody>
+          </table>
+          <button className="add">Add Task</button>
+        </div>
+        <div className="subject">
+          <table>
+            <thead>
+              <tr>
+                <th colSpan={5}>ST2334</th>
+              </tr>
+              <tr>
+                <th>Name</th>
+                <th>Deadline</th>
+                <th>Priority</th>
+                <th>Progress</th>
+                <th>isCompleted</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Task 1</td>
+                <td>25/08/25</td>
+                <td>High</td>
+                <td>Not Started</td>
+                <td>False</td>
+              </tr>
+              <tr>
+                <td>Task 2</td>
+                <td>25/08/25</td>
+                <td>High</td>
+                <td>In Progress</td>
+                <td>False</td>
+              </tr>
+              <tr>
+                <td>Task 3</td>
+                <td>25/08/25</td>
+                <td>High</td>
+                <td>Finished</td>
+                <td>True</td>
+              </tr>
+            </tbody>
+          </table>
+          <button className="add">Add Task</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
